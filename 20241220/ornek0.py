fbb9905c34dd2f9b646e6444ca77d3080985b65d
tasarimkodlama <86604381+tasarimkodlama@users.noreply.github.com>
2024-12-20 11:56:28 +0300

    Add files via upload

---
+++ 20241220/ornek0.py
@@ -0,0 +1,7 @@
+not1 =  int(input("1. Sınav Notu :"))
+not2 =  int(input("2. Sınav Notu :"))
+ortalama = (not1 + not2)/2
+
+print=("Sınav notları ortalaması :",ortalama)
+
+
---
+++ 20241220/ornek2.py
@@ -0,0 +1,6 @@
+kisa_kenar=int(input("Kısa kenarı giriniz: "))
+uzun_kenar=int(input("Uzun kenarı giriniz: "))
+cevre=(kisa_kenar+uzun_kenar)*2 #kisa_kenar+uzun_kenar+kisa_kenar+uzun_kenar
+alan=kisa_kenar*uzun_kenar
+print("Dikdörtgenin çevresi: ", cevre)
+print("Dikdörtgenin alanı: ", alan)
